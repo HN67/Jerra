d02f0d9dd8676f25c8f00a035446eac9c2258437
HN67 <hn67allardkids@gmail.com>
2020-02-11 21:43:49 -0700

    Upgrade Player to be able to create a useful Projectile

--- src/Player.java
+++ src/Player.java
@@ -3,30 +3,64 @@
  */
 public class Player extends DefaultEntity {
 
+    // Temporary variable, should be moved to config or something?
+    private int bulletLife = 5;
+
+    private String direction;
+
     public Player(Vector position, Vector velocity) {
         super(position, velocity);
+        this.direction = "UP";
     }
 
     @Override
     public void update(String command) {
         if (command.equals("up")) {
             this.setVelocity(0, -1);
+            this.direction = "UP";
         }
         if (command.equals("down")) {
             this.setVelocity(0, 1);
+            this.direction = "DOWN";
         }
         if (command.equals("left")) {
             this.setVelocity(-1, 0);
+            this.direction = "LEFT";
         }
         if (command.equals("right")) {
             this.setVelocity(1, 0);
+            this.direction = "RIGHT";
         }
         super.update(command);
     }
 
     @Override
     public String getName() {
-        return "PLAYER";
+        // Indicate direction in name
+        return "PLAYER (" + this.direction + ")";
+    }
+
+    /**
+     * Returns a new projectile, shot in the direction the Player is facing
+     * @return A Projectile, that has not been added to any room
+     */
+    public Projectile shoot() {
+        // Make sure to delink position vectors
+        // Set Projectile velocity based on facing direction
+        if (this.direction.equals("UP")) {
+            return new Projectile(new Vector(this.getPosition()), new Vector(0, -1), this.bulletLife);
+        }
+        if (this.direction.equals("DOWN")) {
+            return new Projectile(new Vector(this.getPosition()), new Vector(0, 1), this.bulletLife);
+        }
+        if (this.direction.equals("RIGHT")) {
+            return new Projectile(new Vector(this.getPosition()), new Vector(1, 0), this.bulletLife);
+        }
+        if (this.direction.equals("LEFT")) {
+            return new Projectile(new Vector(this.getPosition()), new Vector(-1, 0), this.bulletLife);
+        }
+        // If somehow there is no direction, just drop the projectile
+        return new Projectile(new Vector(this.getPosition()), new Vector(0, 0), this.bulletLife);
     }
     
 }
