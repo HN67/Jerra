eb4d8df5a39a7a5e5b147c39347df4c5fb8979fe
Rupert Amodia <rupertraphael@live.com>
2020-02-17 22:10:47 -0700

    Create sample concrete item classes. ☕️
    The descriptions of both items are a single Brooklyn Nine-Nine
    reference 😄.

---
+++ src/Caffeine.java
@@ -0,0 +1,15 @@
+/**
+ * Caffeine
+ * Caffeine speeds you up but...
+ */
+public class Caffeine extends Item{
+
+    public String name() {
+        return "Caffeine";
+    }
+
+    public String description() {
+        return "Caffeine speeds you up but...";
+    }
+
+}
