e84dc766cd3bfe9a67c8eef2cea07d088883adb4
Rupert Amodia <rupertraphael@live.com>
2020-02-17 22:42:43 -0700

    Create sample concrete item classes. ☕️
    The descriptions of both items are a single Brooklyn Nine-Nine
    reference 😄

---
+++ src/HalfAndHalf.java
@@ -0,0 +1,15 @@
+/**
+ * HalfAndHalf
+ * ... slows you down.
+ */
+public class HalfAndHalf extends Item{
+
+    public String name() {
+        return "Half-and-Half";
+    }
+
+    public String description() {
+        return "... but half-and-half slows you down.";
+    }
+
+}
