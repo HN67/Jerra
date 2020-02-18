c1ec209520ea703d3798c61ab2c15253cb6c7708
dol1210 <59849463+dol1210@users.noreply.github.com>
2020-02-17 18:47:56 -0700

    Update Effect.java
    added setters and getters for damage and acceleration

--- src/Effect.java
+++ src/Effect.java
@@ -1,5 +1,13 @@
 
 public interface Effect {
+	
 	public Effect combineEffect(Effect effect);
+	
 	public void applyEffect(Entity entity);
+	
+	public void setDamage(int x);
+	public void getDamage();
+	
+	public void setAcceleration(int x);
+	public void getAcceleration();
 }
