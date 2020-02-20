4409c741857f9d02f0a4711e59b915b1c2eff1a9
HN67 <hn67allardkids@gmail.com>
2020-02-20 11:21:15 -0700

    Update TextRoom to handle collision detection and interaction

--- src/TextRoom.java
+++ src/TextRoom.java
@@ -16,10 +16,12 @@ public class TextRoom implements Room {
         this.spawners = new ArrayList<Spawner>();
     }
 
+    @Override
     public void spawnEntity(Entity entity) {
         this.entityList.add(entity);
     }
 
+    @Override
     public void spawnPlayer(Player player) {
         // Adding to entityList allows bulk updating
         this.entityList.add(player);
@@ -27,22 +29,53 @@ public class TextRoom implements Room {
         this.spawners.add(player);
     }
 
+    @Override
     public void update(String command) {
+
         // Update all general entities
         for (Entity entity: this.entityList) {
             entity.update(command);
         }
+
+        // Check for collisions between Entities (O(n^2))
+        // Creates a list of lists of collisions, ordered in the same order as entity list
+        List<List<Entity>> allCollisions = new ArrayList<List<Entity>>(this.entityList.size());
+        // Iterate through each entity
+        for (Entity entity: this.entityList) {
+            List<Entity> collisions = new ArrayList<Entity>();
+            // Check for collision with *every other* entity
+            for (Entity other: this.entityList) {
+                // Check for collision and ensure its not the same entity
+                // Lazy evaluation ensures second check is only performed if first succeeds
+                if (entity.collides(other) && entity != other) {
+                    collisions.add(other);
+                }
+            }
+            // Add collisions to larger list
+            allCollisions.add(collisions);
+        }
+        // Resolve collisions through interaction
+        for (int i = 0, n = this.entityList.size(); i < n; i++) {
+            // Iterate through each collision with the entity
+            for (Entity other: allCollisions.get(i)) {
+                this.entityList.get(i).interact(other, command);
+            }
+        }
+
         // Remove dead entities
         this.entityList.removeIf(entity -> !entity.alive());
-        // Check players
+        
+        // Check spawners
         for (Spawner spawner: this.spawners) {
             // Get spawned Entity if Spawner spawns
             if (spawner.spawns(command)) {
                 this.spawnEntity(spawner.spawn());
             }
         }
+
     }
 
+    @Override
     public String toString() {
         String output = "";
         for (Entity entity: this.entityList) {
