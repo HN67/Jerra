f27eb5a3fc85a7b95a297d3356e855148ab6d54b
HN67 <hn67allardkids@gmail.com>
2020-02-20 18:42:05 -0700

    Enhance Rect and Vector with documentation and constructors and center getters

--- src/Rect.java
+++ src/Rect.java
@@ -23,6 +23,17 @@ public class Rect {
     }
 
     /**
+     * Constructs a Rect with an origin at (x, y) and size of (width, height).
+     * @param x a int, the x coordinate of this Rect's origin
+     * @param y a int, the y coordinate of this Rect's origin
+     * @param width a int, the width of this Rect's size
+     * @param height a int, the height of this Rect's size
+     */
+    public Rect(int x, int y, int width, int height) {
+        this(new Vector(x, y), new Vector(width, height));
+    }
+
+    /**
      * Constructs a Rect that is identical to the given one
      * @param other a Rect, to be copied
      */
@@ -31,52 +42,164 @@ public class Rect {
         this.size = new Vector(other.size);
     }
 
+    /**
+     * Named constructor to make a Rect using corners
+     * @param topleft a Vector, the coordinates of this Rect's top left corner
+     * @param bottomright a Vector, the coordinates of this Rect's bottom right corner
+     * @return a Rect, constructed based on topleft and bottomright corners
+     */
+    public static Rect fromCorners(Vector topleft, Vector bottomright) {
+        return new Rect(topleft, bottomright.subtract(topleft));
+    }
+
+    /**
+     * Named constructor to make a Rect using edge coordinates
+     * @param left a int, the x coordinate of the left edge
+     * @param top a int, the y coordinate of the top edge
+     * @param right a int, the x coordinate of the right edge
+     * @param bottom a int, the y coordinate of the bottom edge
+     * @return a Rect, constructed based on given edge coordinates
+     */
+    public static Rect fromEdges(int left, int top, int right, int bottom) {
+        return fromCorners(new Vector(left, top), new Vector(right, bottom));
+    }
+    
+    /**
+     * Returns the origin of this Rect, identical to the top left corner
+     * @return a Vector, represents the x,y of the Rect origin / top left
+     */
     public Vector getOrigin() {
         return this.origin;
     }
 
+    /**
+     * Returns the size of this Rect
+     * Note that left + width = right, and top + height = bottom
+     * @return a Vector, whose x is the width, and y the height of this Rect
+     */
     public Vector getSize() {
         return this.size;
     }
 
+    /**
+     * Returns the x coordinate of this Rect's origin.
+     * Identical to Rect.getOrigin().x()
+     * Also identical to the left edge of the Rect.
+     * @return a int, the x coordinate of Rect origin
+     */
     public int x() {
         return origin.x();
     }
 
+    /**
+     * Returns the y coordinate of this Rect's origin.
+     * Identical to Rect.getOrigin().y()
+     * Also identical to the top edge of the Rect
+     * @return a int, the y coordinate of Rect origin.
+     */
     public int y() {
         return origin.y();
     }
 
+    /**
+     * Returns the width of this Rect.
+     * left + width = right.
+     * @return a int, the number of pixels this Rect occupies horizontally
+     */
     public int width() {
         return size.x();
     }
 
+    /**
+     * Returns the height of this Rect.
+     * top + height = bottom
+     * @return a int, the number of pixels this Rect occupies vertically
+     */
     public int height() {
         return size.y();
     }
 
+    /**
+     * Returns the y coordinate of the top edge of this Rect.
+     * The bounded area of this Rect is below this line.
+     * @return a int, the y coordinate of this Rect's top edge.
+     */
     public int top() {
         return origin.y();
     }
 
+    /**
+     * Returns the x coordinate of the left edge of this Rect.
+     * The bounded area of this Rect is to the right of this line.
+     * @return a int, the x coordinate of this Rect's left edge.
+     */
     public int left() {
         return origin.x();
     }
 
+    /**
+     * Returns the x coordinate of the right edge of this Rect.
+     * The bounded area of this Rect is to the left of this line.
+     * @return a int, the x coordinate of this Rect's right edge.
+     */
     public int right() {
         return origin.x() + this.width();
     }
 
+    /**
+     * Returns the y coordinate of the bottom edge of this Rect.
+     * The bounded area of this Rect above this line.
+     * @return a int, the y coordinate of this Rect's bottom edge.
+     */
     public int bottom() {
         return origin.y() + this.height();
     }
 
+    /**
+     * Returns the topleft point of this Rect.
+     * Has the same data as top and left methods
+     * Identical to the origin of the Rect.
+     * @return a Vector, whose x is the left edge and y is top edge of this Rect.
+     */
     public Vector topleft() {
         return this.origin;
     }
 
+    /**
+     * Returns the bottomright point of this Rect.
+     * Has the same data as bottom and right methods
+     * Identical to the origin of the Rect plus the size.
+     * @return a Vector, whose x is the right edge and y is bottom edge of this Rect.
+     */
     public Vector bottomright() {
         return this.origin.add(this.size);
+    }
+
+    /**
+     * Returns the center of this Rect as a Vector.
+     * Has data identical to centerX and centerY calls, packaged into a Vector
+     * @return a Vector, the average of this Rect's edges, truncated
+     */
+    public Vector center() {
+        return new Vector(this.centerX(), this.centerY());
+    }
+
+    /**
+     * Returns the x coordinate of the center of this Rect.
+     * May behave strangly for small Rects
+     * @return a int, the average of the left and right edge of this Rect, truncated
+     */
+    public int centerX() {
+        return (this.left() + this.right()) / 2;
+    }
+
+    /**
+     * Returns the y coordinate of the center of this Rect.
+     * May behave strangly for small Rects
+     * @return a int, the average of the top and bottom edge of this Rect, truncated
+     */
+    public int centerY() {
+        return (this.top() + this.bottom()) / 2;
     }
 
     /**
