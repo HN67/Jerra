d3b66cee63f5e4168c6fe5bef24c29291766c976
Rupert Amodia <rupertraphael@live.com>
2020-03-03 00:53:03 -0700

    Extract Wall deflection logic into Deflects interface.

---
+++ src/jerra/entity/Deflects.java
@@ -0,0 +1,76 @@
+package jerra.entity;
+
+import jerra.core.Rect;
+
+public interface Deflects extends Entity {
+    default void deflect(Entity other) {
+        // Stop the other entity.
+        other.getPresence().getVelocity().scale(0);
+
+        Rect otherPosition = other.getPosition();
+
+        // If another entity collides against the wall's right side,
+        // then align the other entity's left side against 
+        // the right side of the wall.
+        if(
+            otherPosition.left() <= this.getPosition().right() &&
+            otherPosition.left() > this.getPosition().centerX() &&
+            other.getPresence().getVelocity().x() < 0
+        ) {
+            other.getPresence().setPosition(
+                otherPosition.alignLeft(this.getPosition().right())
+            );  
+            return;
+        }
+
+        // If another entity collides against the wall's left side,
+        // then align the other entity's right side against 
+        // the left side of the wall.
+        if(
+            otherPosition.right() >= this.getPosition().left() &&
+            otherPosition.right() < this.getPosition().centerX() &&
+            other.getPresence().getVelocity().x() > 0
+        ) {
+            other.getPresence().setPosition(
+                otherPosition.alignRight(this.getPosition().left())
+            );
+            return;
+        }
+
+        // If another entity collides against the wall's top side,
+        // then align the other entity's bottom side against 
+        // the right top of the wall.
+        if(
+            otherPosition.bottom() >= this.getPosition().top() && 
+            otherPosition.bottom() < this.getPosition().centerY() && 
+            other.getPresence().getVelocity().y() > 0
+        ) {
+            other.getPresence().setPosition(
+                otherPosition.alignBottom(this.getPosition().top())
+            );
+            return;
+        }
+
+        // If another entity collides against the wall's bottom side,
+        // then align the other entity's top side against 
+        // the bottom side of the wall.
+        if(
+            otherPosition.top() <= this.getPosition().bottom() && 
+            otherPosition.top() > this.getPosition().centerY() &&
+            other.getPresence().getVelocity().y() < 0
+        ) {
+            other.getPresence().setPosition(
+                otherPosition.alignTop(this.getPosition().bottom())
+            );
+            return;
+        }
+    }
+
+    /**
+     * Prevents another entity from going through the wall.
+     */
+    @Override
+    default void interact(Entity other) {
+        this.deflect(other);
+    }
+}
--- src/jerra/entity/Wall.java
+++ src/jerra/entity/Wall.java
@@ -1,12 +1,11 @@
 package jerra.entity;
 
-import jerra.core.Rect;
 import jerra.presence.Presence;
 
 /**
  * Wall
  */
-public class Wall extends DefaultEntity{
+public class Wall extends DefaultEntity implements Deflects {
     
     public Wall(Presence presence) {
         super(presence);
@@ -21,72 +20,10 @@ public class Wall extends DefaultEntity{
             ")";
     }
 
-    /**
-     * Prevents another entity from going through the wall.
-     */
     @Override
     public void interact(Entity other) {
-        // Stop the other entity.
-        other.getPresence().getVelocity().scale(0);
+        this.deflect(other);
 
-        Rect otherPosition = other.getPosition();
-
-        // If another entity collides against the wall's right side,
-        // then align the other entity's left side against 
-        // the right side of the wall.
-        if(
-            otherPosition.left() <= this.getPosition().right() &&
-            otherPosition.left() > this.getPosition().centerX() &&
-            other.getPresence().getVelocity().x() < 0
-        ) {
-            other.getPresence().setPosition(
-                otherPosition.alignLeft(this.getPosition().right())
-            );  
-            return;
-        }
-
-        // If another entity collides against the wall's left side,
-        // then align the other entity's right side against 
-        // the left side of the wall.
-        if(
-            otherPosition.right() >= this.getPosition().left() &&
-            otherPosition.right() < this.getPosition().centerX() &&
-            other.getPresence().getVelocity().x() > 0
-        ) {
-            other.getPresence().setPosition(
-                otherPosition.alignRight(this.getPosition().left())
-            );
-            return;
-        }
-
-        // If another entity collides against the wall's top side,
-        // then align the other entity's bottom side against 
-        // the right top of the wall.
-        if(
-            otherPosition.bottom() >= this.getPosition().top() && 
-            otherPosition.bottom() < this.getPosition().centerY() && 
-            other.getPresence().getVelocity().y() > 0
-        ) {
-            other.getPresence().setPosition(
-                otherPosition.alignBottom(this.getPosition().top())
-            );
-            return;
-        }
-
-        // If another entity collides against the wall's bottom side,
-        // then align the other entity's top side against 
-        // the bottom side of the wall.
-        if(
-            otherPosition.top() <= this.getPosition().bottom() && 
-            otherPosition.top() > this.getPosition().centerY() &&
-            other.getPresence().getVelocity().y() < 0
-        ) {
-            other.getPresence().setPosition(
-                otherPosition.alignTop(this.getPosition().bottom())
-            );
-            return;
-        }
-
-        
+        super.interact(other);
     }
 }
