88cd31955af650382644bfc5153480ebb6e8a6b9
dol1210 <59849463+dol1210@users.noreply.github.com>
2020-03-03 14:29:27 -0700

    added comment code on GraphicsView for future image implementation

--- src/jerra/view/GraphicView.java
+++ src/jerra/view/GraphicView.java
@@ -62,6 +62,7 @@ public class GraphicView extends View<Room> {
 				width,
 				height
 			);
+//			context.drawImage(entity.image(), entity.getPosition().centerX(), entity.getPosition().centerY());
 		}
 	}
 
