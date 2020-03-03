760c3d7fd6363c81f9d5f624d554a0e3c2b9b0b3
Rupert Amodia <rupertraphael@live.com>
2020-03-02 23:33:31 -0700

    Write comments for Wall.interact().

--- src/jerra/entity/Wall.java
+++ src/jerra/entity/Wall.java
@@ -7,22 +7,33 @@ import jerra.presence.Presence;
  * Wall
  */
 public class Wall extends DefaultEntity{
+    
     public Wall(Presence presence) {
         super(presence);
     }
 
     @Override
     public String getName() {
-        return "WALL";
+        return "WALL (" + 
+            this.getPosition().height()  + 
+            "," +
+            this.getPosition().width()  +  
+            ")";
     }
 
+    /**
+     * Prevents another entity from going through the wall.
+     */
     @Override
     public void interact(Entity other) {
-        // Stop other entity.
+        // Stop the other entity.
         other.getPresence().getVelocity().scale(0);
 
         Rect otherPosition = other.getPosition();
 
+        // If another entity collides against the wall's right side,
+        // then align the other entity's left side against 
+        // the right side of the wall.
         if(
             otherPosition.left() <= this.getPosition().right() &&
             otherPosition.left() > this.getPosition().centerX() &&
@@ -34,21 +45,27 @@ public class Wall extends DefaultEntity{
             return;
         }
 
+        // If another entity collides against the wall's left side,
+        // then align the other entity's right side against 
+        // the left side of the wall.
         if(
             otherPosition.right() >= this.getPosition().left() &&
             otherPosition.right() < this.getPosition().centerX() &&
             other.getPresence().getVelocity().x() > 0
         ) {
-            System.out.println("aligntt right");
             other.getPresence().setPosition(
                 otherPosition.alignRight(this.getPosition().left())
             );
             return;
         }
 
+        // If another entity collides against the wall's top side,
+        // then align the other entity's bottom side against 
+        // the right top of the wall.
         if(
             otherPosition.bottom() >= this.getPosition().top() && 
-            otherPosition.bottom() < this.getPosition().centerY()
+            otherPosition.bottom() < this.getPosition().centerY() && 
+            other.getPresence().getVelocity().y() > 0
         ) {
             other.getPresence().setPosition(
                 otherPosition.alignBottom(this.getPosition().top())
@@ -56,9 +73,13 @@ public class Wall extends DefaultEntity{
             return;
         }
 
+        // If another entity collides against the wall's bottom side,
+        // then align the other entity's top side against 
+        // the bottom side of the wall.
         if(
             otherPosition.top() <= this.getPosition().bottom() && 
-            otherPosition.top() > this.getPosition().centerY()
+            otherPosition.top() > this.getPosition().centerY() &&
+            other.getPresence().getVelocity().y() < 0
         ) {
             other.getPresence().setPosition(
                 otherPosition.alignTop(this.getPosition().bottom())
