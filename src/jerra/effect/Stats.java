1888502aa2386b01b6be05c3933f300df22948f3
HN67 <hn67allardkids@gmail.com>
2020-03-05 11:30:52 -0700

    Create Stats class

---
+++ src/jerra/effect/Stats.java
@@ -0,0 +1,31 @@
+package jerra.effect;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Stats class that contains various types of stats
+ */
+public class Stats {
+
+    private Map<Type, Integer> values;
+
+    public enum Type {
+        HEALTH,
+        STRENGTH,
+        FORTITUDE,
+    }
+
+    public Stats() {
+        this.values = new HashMap<Type, Integer>(Type.values().length);
+    }
+
+    public void setValue(Type stat, int value) {
+        this.values.put(stat, value);
+    }
+
+    public void getValue(Type stat) {
+        this.values.get(stat);
+    }
+    
+}
