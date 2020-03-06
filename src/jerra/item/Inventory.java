dbf4a91288eb8a4de6cea11b38307e7869934ee1
Jacob <57515453+jaewon-ma@users.noreply.github.com>
2020-03-05 23:36:55 -0700

    Update Inventory.java

--- src/jerra/item/Inventory.java
+++ src/jerra/item/Inventory.java
@@ -6,35 +6,36 @@ public class Inventory{
 
     private HashMap<Item, Integer> inventory = new HashMap<Item, Integer>();
     
-public Inventory() {
-    
-}
 
-public String toString() {
-    return this.inventory.toString();
-}
 
-public void add(Item item) {
-    if (inventory.size() > 0) {  
-        for (Item i : inventory.keySet()) {
-            if (i.equals(item)) {
-                inventory.replace(i, inventory.get(i)+1);
-                return;
+    public String toString() {
+        return this.inventory.toString();
+    }
+
+    public void add(Item item) {
+        if (inventory.size() > 0) {  
+            for (Item i : inventory.keySet()) {
+                if (i.equals(item)) {
+                    inventory.replace(i, inventory.get(i)+1);
+                    return;
+                }
+                
             }
-            
+            inventory.put(item, 1);
+            return;
         }
-        inventory.put(item, 1);
-        return;
+        else {
+            inventory.put(item, 1);
+        }
     }
-    else {
-        inventory.put(item, 1);
-    }
-}
 
-public void remove(Item item) {
-    if (inventory.get(item) > 0) {
-        inventory.replace(item, inventory.get(item)-1);
+    public void remove(Item item) {  
+            for (Item i : inventory.keySet()) {
+                if (i.equals(item)) {
+                    inventory.replace(i, inventory.get(i)-1);
+                    return;
+                }   
+            }
     }
-}
 
 }
