e83b6e749c10300d5ee729a3f6c444b9b622b2bd
Rupert Amodia <rupertraphael@live.com>
2020-03-13 21:37:09 -0600

    Refactor .equals for Item and add tests.

---
+++ src/tests/ItemTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import jerra.item.Caffeine;
+import jerra.item.HalfAndHalf;
+import jerra.item.Item;
+
+/**
+ * Test Class for testing the Rect class.
+ */
+public class ItemTest {
+
+    @Test
+    public void test_SameItemsAreEqual() {
+        Item coffee = new Caffeine();
+        Item coffee2 = new Caffeine();
+
+        Assert.assertEquals("Comparing two Caffeine objects if equal, expecting true", true, coffee.equals(coffee2));
+    }
+
+    @Test
+    public void test_DifferentItemsAreNotEqual() {
+        Item coffee = new Caffeine();
+        Item half = new HalfAndHalf();
+
+        Assert.assertEquals("Comparing Caffeine and HalfAndHalf objects if equal, expecting true", false, coffee.equals(half));
+    }
+
+}
