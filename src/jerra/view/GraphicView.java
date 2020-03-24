7e24ec2a0c8ad6cce5f017266b8a73c8cf99b990
Rupert Amodia <rupertraphael@live.com>
2020-03-23 21:13:07 -0600

    Create and use Healthbar view.

--- src/jerra/view/GraphicView.java
+++ src/jerra/view/GraphicView.java
@@ -1,5 +1,6 @@
 package jerra.view;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javafx.scene.canvas.Canvas;
@@ -7,24 +8,25 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import jerra.core.Rect;
+import jerra.entity.DefaultCharacter;
 import jerra.entity.Entity;
+import jerra.entity.Player;
 import jerra.room.Room;
-import jerra.stats.Character;
-import jerra.stats.Stats.Type;
 
 public class GraphicView extends View<Room> {
 
 	private Map<String, Image> imageDictionary;
 
-	private int tick = 0;
+	private Map<Entity, Healthbar> healthbars;
 	
 	private Canvas canvas;
 	
 	public GraphicView(Room model, Canvas canvas, Map<String, Image> imageDictionary) {
 		super(model);
 		
-		this.canvas = canvas;
 		this.imageDictionary = imageDictionary;
+		this.healthbars = new HashMap<Entity, Healthbar>();
+		this.canvas = canvas;
 	}
 	
 	public Canvas getCanvas() {
@@ -63,38 +65,34 @@ public class GraphicView extends View<Room> {
 
 			Image image = this.imageDictionary.get(entity.image());
 
-			context.setFill(Color.RED);
-
-			// entity.getStats().setOnChangeValue((event, type) -> context.setFill(Color.YELLOW));
-
-			this.showHealthBar(entity, context);
+			this.drawHealthbar(entity, context);
 			
 			// Align center of image and position
 			context.drawImage(image, pos.centerX() - image.getWidth()/2, pos.centerY() - image.getHeight()/2);
-		
-			tick++;
 
+			this.healthbars.entrySet().removeIf(healthbar -> healthbar.getValue().equals(null));
 		}
 	}
 
-	private void showHealthBar(Entity entity, GraphicsContext context) {
-		if(this.tick % 60 > 30) {
-			return;
-		}
+	private void drawHealthbar(Entity entity, GraphicsContext context) {
+		if(entity instanceof DefaultCharacter) {
+			if(!this.healthbars.containsKey(entity)) {
 
-		if (entity.symbol().equals("P")) {
-			Character character = (Character) entity;
-//				context.setFill(Color.WHITE);
-//				context.fillRect(entity.getPosition().left(), entity.getPosition().centerY() - 25, entity.getPosition().width(), 4);
-			context.setFill(Color.GREEN);
-			context.fillRect(entity.getPosition().left(), entity.getPosition().centerY() - 25, entity.getPosition().width()*character.getStats().getValue(Type.HEALTH)/character.getStats().getValue(Type.VITALITY), 4);
-		} else if (entity.symbol().equals("E")) {
-			System.out.println("Entity health changed");
-			Character character = (Character) entity;
-//				context.setFill(Color.WHITE);
-//				context.fillRect(entity.getPosition().left(), entity.getPosition().centerY() - 25, entity.getPosition().width(), 4);
-			context.setFill(Color.RED);
-			context.fillRect(entity.getPosition().left(), entity.getPosition().centerY() - 25, entity.getPosition().width()*character.getStats().getValue(Type.HEALTH)/character.getStats().getValue(Type.VITALITY), 4);
+				Color color = Color.RED;
+
+				if(entity instanceof Player) {
+					color = Color.GREEN;
+				}
+
+				this.healthbars.put(entity, new Healthbar((DefaultCharacter) entity, canvas, color, 100));
+			}
+
+			this.healthbars.get(entity).render();
+			
+			entity.getStats().setOnChangeValue(
+				(event, type) -> 
+				this.healthbars.get(entity).show()
+			);
 		}
 	}
 
